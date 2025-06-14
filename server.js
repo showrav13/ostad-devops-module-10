d6950b8b6346855c2d48a9ef6a833b16c126a77a
Showrav Acharjee <43024533+showrav-iplus@users.noreply.github.com>
2025-06-14 22:56:16 +0600

    updated version

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const VERSION = "Version 2";
+const VERSION = "Version 3";
 
 app.get('/', (req, res) => {
   res.send(`Hello from ${VERSION}`);
